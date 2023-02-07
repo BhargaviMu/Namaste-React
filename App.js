d5076568ba0270944137bf299410b6534b94b1ff
Vinita <vinitaswamy11@gmail.com>
2023-02-07 19:09:33 +0530

    added first leacture

---
+++ App.js
@@ -0,0 +1,43 @@
+//  Using JS to manipulate the DOM
+
+// const heading = document.createElement("h1");
+
+// heading.innerHTML = "Namaste Everyone";
+
+// const root = document.getElementById("root");
+
+// root.appendChild(heading);
+
+
+
+// Using React 
+
+const heading = React.createElement(
+    "h1", 
+    {
+        id: "title"
+    }, 
+    "Namaste Everyone"
+);
+
+const headingOne = React.createElement(
+    "h2", 
+    {
+        id: "title",
+        className: "class"
+    }, 
+    "Namaste Heading One"
+);
+
+const headingMain = React.createElement(
+    "div", 
+    {
+        id: "container"                
+    }, 
+    [heading,headingOne]
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(headingMain);
+
